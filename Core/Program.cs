3efd1648fe29c69424d9e4eae68846119672b010
Morten Kristensen <mokr@mapspeople.com>
2020-11-17 13:38:26 +0100

    Added some debuffing shit.

--- Core/Program.cs
+++ Core/Program.cs
@@ -1,10 +1,19 @@
 ﻿using System;
+using Core.Database;
+using Core.Models;
+using System.IO;
 
 namespace Core {
     class Program {
         static void Main(string[] args) {
-            DBTester dBTester = new DBTester();
-            dBTester.PutStuffInDB();
+            DBAccess db = new DBAccess();
+            string code = File.ReadAllText(@"C:\Mapspeople\HelloWorld\main.py");
+            Script script = new Script() {
+                Id = "2", Customer = "Hans", Language = "python", LanguageVersion = "3.8", Name = "Hello", ScriptVersion = 1.0,
+                Code = code
+            };
+            db.Upsert(script);
+            
         }
     }
 }
