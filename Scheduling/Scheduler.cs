7902c953f44bad7f9c6cf5a7510ada325d5df7bb
Heidi Kim Thomsen <1074310@ucn.dk>
2020-12-02 09:12:13 +0100

    created class scheduler

---
+++ Scheduling/Scheduler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace Scheduling {
+    public class Scheduler : IHostedService {
+        
+    }
+    public Task StartAsync(CancellationToken cancellationToken) {
+
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken) {
+
+    }
+}
