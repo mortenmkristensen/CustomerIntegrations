3728e73d1b59a46a047e05d5da62b21e176d2953
Zhiru Cao <1074160@ucn.dk>
2020-12-02 10:14:48 +0100

    Scheduler GetIds og SendIdsToRabbitMQ

--- Scheduling/Scheduler.cs
+++ Scheduling/Scheduler.cs
@@ -7,13 +7,173 @@ using Microsoft.Extensions.Hosting;
 
 namespace Scheduling {
     public class Scheduler : IHostedService {
-        
-    }
-    public Task StartAsync(CancellationToken cancellationToken) {
+        IDBAccess dbAccess = new DBAccess(new DBConfig());
+        Timer _timer;
+        List<Script> rubyScripts = new List<Script>();
+        List<Script> pythonScripts = new List<Script>();
+        List<Script> javaScriptScripts = new List<Script>();
+        List<string> rubyIds = new List<string>();
+        List<string> pythonIds = new List<string>();
+        List<string> javaScriptIds = new List<string>();
+        public Task StartAsync(CancellationToken cancellationToken) {
+            _timer = new Timer(
+                SendIdsToRabbitMQ,
+                null,
+                TimeSpan.Zero,
+                TimeSpan.FromSeconds(5)
+                );
+            return Task.CompletedTask;
+        }
 
-    }
+        public Task StopAsync(CancellationToken cancellationToken) {
+            _timer?.Change(Timeout.Infinite, 0);
 
-    public Task StopAsync(CancellationToken cancellationToken) {
+            return Task.CompletedTask;
 
+        }
+
+        private void GetIds() {
+            IEnumerable<Script> scripts = dbAccess.GetAll();
+            foreach (var script in scripts) {
+                if (script.Language.Equals("ruby")) {
+                    rubyScripts.Add(script);
+                }
+                if (script.Language.Equals("python")) {
+                    pythonScripts.Add(script);
+                }
+                if (script.Language.Equals("javaScript")) {
+                    javaScriptScripts.Add(script);
+                }
+
+            }
+
+            foreach (var rubyScript in rubyScripts) {
+                rubyIds.Add(rubyScript.Id);
+            }
+
+            foreach (var pythonScript in pythonScripts) {
+                pythonIds.Add(pythonScript.Id);
+            }
+
+            foreach (var javaScriptScript in javaScriptScripts) {
+                javaScriptIds.Add(javaScriptScript.Id);
+            }
+
+        }
+
+        private void SendIdsToRabbitMQ(object state) {
+            GetIds();
+            try {
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel()) {
+                    channel.QueueDeclare(queue: "RubyIds_Queue",
+                                         durable: true,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    var message = JsonConvert.SerializeObject(rubyIds);
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    var properties = channel.CreateBasicProperties();
+                    properties.Persistent = true;
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "RubyIds_Queue",
+                                         basicProperties: properties,
+                                         body: body);
+                    Console.WriteLine(message);
+                    Console.WriteLine();
+                    Console.WriteLine();
+                }
+            } catch (Exception e) {
+                if (e is AlreadyClosedException) {
+                    Console.WriteLine("The connectionis already closed");
+                } else if (e is BrokerUnreachableException) {
+                    Console.WriteLine("The broker cannot be reached");
+                } else if (e is OperationInterruptedException) {
+                    Console.WriteLine("The operation was interupted");
+                } else if (e is ConnectFailureException) {
+                    Console.WriteLine("Could not connect to the broker broker");
+                } else {
+                    Console.WriteLine("Something went wrong");
+                }
+            }
+            try {
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel()) {
+                    channel.QueueDeclare(queue: "PythonIds_Queue",
+                                         durable: true,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    var message = JsonConvert.SerializeObject(pythonIds);
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    var properties = channel.CreateBasicProperties();
+                    properties.Persistent = true;
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "PythonIds_Queue",
+                                         basicProperties: properties,
+                                         body: body);
+                    Console.WriteLine(message);
+                    Console.WriteLine();
+                    Console.WriteLine();
+                }
+            } catch (Exception e) {
+                if (e is AlreadyClosedException) {
+                    Console.WriteLine("The connectionis already closed");
+                } else if (e is BrokerUnreachableException) {
+                    Console.WriteLine("The broker cannot be reached");
+                } else if (e is OperationInterruptedException) {
+                    Console.WriteLine("The operation was interupted");
+                } else if (e is ConnectFailureException) {
+                    Console.WriteLine("Could not connect to the broker broker");
+                } else {
+                    Console.WriteLine("Something went wrong");
+                }
+            }
+            try {
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel()) {
+                    channel.QueueDeclare(queue: "JavaScriptIds_Queue",
+                                         durable: true,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    var message = JsonConvert.SerializeObject(javaScriptIds);
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    var properties = channel.CreateBasicProperties();
+                    properties.Persistent = true;
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "JavaScriptIds_Queue",
+                                         basicProperties: properties,
+                                         body: body);
+                    Console.WriteLine(message);
+                    Console.WriteLine();
+                    Console.WriteLine();
+                }
+            } catch (Exception e) {
+                if (e is AlreadyClosedException) {
+                    Console.WriteLine("The connectionis already closed");
+                } else if (e is BrokerUnreachableException) {
+                    Console.WriteLine("The broker cannot be reached");
+                } else if (e is OperationInterruptedException) {
+                    Console.WriteLine("The operation was interupted");
+                } else if (e is ConnectFailureException) {
+                    Console.WriteLine("Could not connect to the broker broker");
+                } else {
+                    Console.WriteLine("Something went wrong");
+                }
+            }
+        }
     }
 }
