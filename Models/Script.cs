4d4ebb03d4692b8540d915380b96b5c2040c80d4
Heidi Kim Thomsen <1074310@ucn.dk>
2020-12-09 13:19:50 +0100

    changed model script property from Id to _id

--- Models/Script.cs
+++ Models/Script.cs
@@ -2,7 +2,7 @@
 
 namespace Models {
     public class Script {
-        public string Id { get; set; }
+        public string _id { get; set; }
         public string Name { get; set; }
         public string Customer { get; set; }
         public double ScriptVersion { get; set; }
