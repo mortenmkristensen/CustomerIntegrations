706a7400a393159095ea8e54e6028d8fa2d85f42
Zhiru Cao <1074160@ucn.dk>
2021-01-04 12:31:45 +0100

    coretest

---
+++ Coretest/AppTest.cs
@@ -0,0 +1,16 @@
+using System;
+using Moq;
+using Xunit;
+
+
+
+namespace Tests.Coretest {
+    public class AppTest {
+        private readonly App app;
+        private readonly Mock<IStager> stagerMock;
+        [Fact]
+        public void Test1() {
+
+        }
+    }
+}
