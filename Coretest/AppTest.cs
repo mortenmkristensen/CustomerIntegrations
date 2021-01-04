351ebf14265caa2899530a7d28674f878a67c028
Heidi Kim Thomsen <1074310@ucn.dk>
2021-01-04 15:35:54 +0100

    some

--- Coretest/AppTest.cs
+++ Coretest/AppTest.cs
@@ -6,7 +6,7 @@ using Xunit;
 
 namespace Tests.Coretest {
     public class AppTest {
-        private readonly App app;
+        private readonly Mock.Of<IApp> app;
         private readonly Mock<IStager> stagerMock;
         [Fact]
         public void Test1() {
