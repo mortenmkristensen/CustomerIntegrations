df5eb802c04d8b33c092ffada6eaa67cdfd4d688
Zhiru Cao <1074160@ucn.dk>
2021-01-06 09:57:16 +0100

    apptest

--- Coretest/AppTest.cs
+++ Coretest/AppTest.cs
@@ -21,9 +21,12 @@ namespace CoreTest {
             app = new App(dbAccessMock.Object, stagerMock.Object, scriptRunnerMock.Object, messageBrokerMock.Object, dataValidationMock.Object);
         }
 
+        //This test tests a list with a correct script.
         [Fact]
         public void RunTest() {
+            //Arrange
             List<Script> scripts1 = new List<Script>();
+            //correct Script
             Script script1 = new Script() {
                 Id = "1",
                 Name = "Python1",
@@ -39,143 +42,13 @@ namespace CoreTest {
                 Author = "test1",
                 LastModified = DateTime.Now
             };
-            Script script2 = new Script() {
-                Id = "2",
-                Name = "Python2",
-                Customer = "testCustomer",
-                ScriptVersion = "1.11",
-                Language = "python",
-                LanguageVersion = "1.0.1",
-                Code = "#This is the datamodel for Location, Source, and State." + "\n" +
-                       "location = { 'Id':" + '\u0022' + "" + '\u0022' + "," + "'ParentId':" + '\u0022' + "" + '\u0022' + "," + "'ExternalId':" + '\u0022' + "" + '\u0022' + "," + "'ConsumerId': 2, 'Sources': [] }" + "\n" +
-                       "source = { 'Type':" + '\u0022' + "" + '\u0022' + "," + "'TimeStamp':" + '\u0022' + "" + '\u0022' + "," + "'State': [] }" + "\n" +
-                        "state = { 'Property':" + '\u0022' + "" + '\u0022' + "," + "'Value':" + '\u0022' + "" + '\u0022' + "," + "}",
-                DateCreated = DateTime.Now,
-                Author = "test1",
-                LastModified = DateTime.Now
-            };
-            Script script3 = new Script() {
-                Id = "3",
-                Name = "ruby1",
-                Customer = "testCustomer",
-                ScriptVersion = "2.11",
-                Language = "ruby",
-                LanguageVersion = "2.0.1",
-                Code = "#This is the datamodel for Location, Source, and State." + "\n" + "class Location" + "\n" +
-                        " attr_writer :Id,:ParentId,:ExternalId,:ConsumerId,:Sources" + "\n" +
-                        " attr_reader :Id,:ParentId,:ExternalId,:ConsumerId,:Sources" + "\n" +
-                        " def to_json(*a)" + "\n" +
-                        "  { 'Id' => @Id, 'ParentId' => @ParentId, 'ExternalId' => @ExternalId, 'ConsumerId'=> @ConsumerId, 'Sources' => @Sources}.to_json" + "\n" +
-                         " end" + "\n" +
-                        " def self.from_json string" + "\n" +
-                        "  data = JSON.load string" + "\n" +
-                        "  self.new data['Id'], data['ParentId'], data['ExternalId'], data['ConsumerId'], data['Sources']" + "\n" +
-                        " end",
-                DateCreated = DateTime.Now,
-                Author = "test1",
-                LastModified = DateTime.Now
-            };
-            Script script4 = new Script() {
-                Id = "4",
-                Name = "ruby1",
-                Customer = "testCustomer",
-                ScriptVersion = "2.11",
-                Language = "ruby",
-                LanguageVersion = "2.0.1",
-                Code = "#This is the datamodel for Location, Source, and State." + "\n" + "class Location" + "\n" +
-                       " attr_writer :Id,:ParentId,:ExternalId,:ConsumerId,:Sources" + "\n" +
-                       " attr_reader :Id,:ParentId,:ExternalId,:ConsumerId,:Sources" + "\n" +
-                       " def to_json(*a)" + "\n" +
-                       "  { 'Id' => @Id, 'ParentId' => @ParentId, 'ExternalId' => @ExternalId, 'ConsumerId'=> @ConsumerId, 'Sources' => @Sources}.to_json" + "\n" +
-                        " end",
-                DateCreated = DateTime.Now,
-                Author = "test1",
-                LastModified = DateTime.Now
-            };
-            Script script5 = new Script() {
-                Id = "5",
-                Name = "javascript1",
-                Customer = "testCustomer",
-                ScriptVersion = "3.11",
-                Language = "javascript",
-                LanguageVersion = "1.0.1",
-                Code = "//This is the datamodel for Location, Source, and State." + "\n" +
-                      "let location = {" + "\n" +
-                      "   Id:" + '\u0022' + "" + '\u0022' + "," + "\n" +
-                      "   ParentId:" + '\u0022' + "" + '\u0022' + "," + "\n" +
-                      "   ExternalId:" + '\u0022' + "" + '\u0022' + "," + "\n" +
-                      "   customerId: 1," + "\n" +
-                      "   Sources: []" + "\n" +
-                      "};",
-                DateCreated = DateTime.Now,
-                Author = "test1",
-                LastModified = DateTime.Now
-            };
-            Script script6 = new Script() {
-                Id = "6",
-                Name = "javascript1",
-                Customer = "testCustomer",
-                ScriptVersion = "3.11",
-                Language = "javascript",
-                LanguageVersion = "1.0.1",
-                Code = "//This is the datamodel for Location, Source, and State." + "\n" +
-                      "let location = {" + "\n" +
-                      "   Id:" + '\u0022' + "" + '\u0022' + "," + "\n" +
-                      "   ParentId:" + '\u0022' + "" + '\u0022' + "," + "\n" +
-                      "   ExternalId:" + '\u0022' + "" + '\u0022' + "," + "\n" +
-                      "   customerId: 1," + "\n" +
-                      "   Sources: []" + "\n" +
-                      "};",
-                DateCreated = DateTime.Now,
-                Author = "test1",
-                LastModified = DateTime.Now
-            };
-            //wrong script
-            Script script7 = new Script() {
-                Id = "7",
-                Name = "javascript1",
-                Customer = "testCustomer",
-                ScriptVersion = "3.11",
-                Language = "javascript",
-                LanguageVersion = "1.0.1",
-                Code = "//This is the datamodel for Location, Source, and State." + "\n" +
-                     "let location = {" + "\n" +
-                     "   Id:" + '\u0022' + "" + '\u0022' + "," + "\n" +
-                     "   ParentId:" + '\u0022' + "" + '\u0022' + "," + "\n" +
-                     "wrong"+
-                     "};",
-                DateCreated = DateTime.Now,
-                Author = "test1",
-                LastModified = DateTime.Now
-            };
-
-            scripts1.Add(script1);
-            scripts1.Add(script2);
-            scripts1.Add(script3);
-            scripts1.Add(script4);
-            scripts1.Add(script5);
-            scripts1.Add(script6);
-            scripts1.Add(script7);
-            string queueName = "MP_QUEUENAME";
+            scripts1.Add(script1);          
+            string queueName = Environment.GetEnvironmentVariable("MP_QUEUENAME");
             messageBrokerMock.Setup(x => x.Receive(queueName)).Returns(scripts1);
             string path1 = $@"c:\scripts\python\{script1.Id}.py";
-            string path2 = $@"c:\scripts\python\{script2.Id}.py";
-            string path3 = $@"c:\scripts\ruby\{script3.Id}.rb";
-            string path4 = $@"c:\scripts\ruby\{script4.Id}.rb";
-            string path5 = $@"c:\scripts\javascript\{script5.Id}.js";
-            string path6 = $@"c:\scripts\javascript\{script6.Id}.js";
-            string path7 = $@"c:\scripts\javascript\{script7.Id}.js";
-
             Dictionary<string, string> paths = new Dictionary<string, string>();
             paths.Add(script1.Id, path1);
-            paths.Add(script2.Id, path2);
-            paths.Add(script3.Id, path3);
-            paths.Add(script4.Id, path4);
-            paths.Add(script5.Id, path5);
-            paths.Add(script6.Id, path6);
-            paths.Add(script7.Id, path7);
             stagerMock.Setup(x => x.GetPaths(scripts1)).Returns(paths);
-            var result2 = "wrong";
             var result = "[{" + '\u0022' + "Id" + '\u0022' + ":" + '\u0022' + "" + '\u0022' +
                 "," + '\u0022' + "ParentId" + '\u0022' + ":" + '\u0022' + "" + '\u0022' + "," + '\u0022' + "ExternalId" + '\u0022'
                 + ":" + '\u0022' + "9894778d - 7ed6 - 4610 - 987a - 033b8125a24e" + '\u0022' + "," +
@@ -187,41 +60,82 @@ namespace CoreTest {
                 ":" + '\u0022' + "SignsOfLife" + '\u0022' + "," + '\u0022' + "Value" + '\u0022' + ":false}]," + '\u0022' + "TimeStamp" + '\u0022' +
                 ":" + '\u0022' + "2020 - 12 - 30T13: 47:32.767689Z" + '\u0022' + "}]}]";
             var interpreterPath = "python";
-            var interpreterPath2 = "ruby";
-            var interpreterPath3 = "node";
-            scriptRunnerMock.Setup(x => x.RunScript(script1.Id, path1, interpreterPath)).Returns(result);
-            scriptRunnerMock.Setup(x => x.RunScript(script2.Id, path2, interpreterPath)).Returns(result);
-            scriptRunnerMock.Setup(x => x.RunScript(script3.Id, path3, interpreterPath2)).Returns(result);
-            scriptRunnerMock.Setup(x => x.RunScript(script4.Id, path4, interpreterPath2)).Returns(result);
-            scriptRunnerMock.Setup(x => x.RunScript(script5.Id, path5, interpreterPath3)).Returns(result);
-            scriptRunnerMock.Setup(x => x.RunScript(script6.Id, path6, interpreterPath3)).Returns(result);
-            scriptRunnerMock.Setup(x => x.RunScript(script7.Id, path7, interpreterPath3)).Returns(result2);
+            scriptRunnerMock.Setup(x => x.RunScript(script1.Id, path1, interpreterPath)).Returns(result);          
             dataValidationMock.Setup(x => x.ValidateScriptOutput(result)).Returns(true);
-            dataValidationMock.Setup(x => x.ValidateScriptOutput(result2)).Throws(new ScriptFailedException());
-
-
-            dbAccessMock.Setup(x => x.Upsert(script1)).Returns(script1);
-            dbAccessMock.Setup(x => x.Upsert(script2)).Returns(script2);
-            dbAccessMock.Setup(x => x.Upsert(script3)).Returns(script3);
-            dbAccessMock.Setup(x => x.Upsert(script4)).Returns(script4);
-            dbAccessMock.Setup(x => x.Upsert(script5)).Returns(script5);
-            dbAccessMock.Setup(x => x.Upsert(script6)).Returns(script6);
-            dbAccessMock.Setup(x => x.Upsert(script7)).Returns(script7);
+            dbAccessMock.Setup(x => x.Upsert(script1)).Returns(script1);          
             List<string> messages = new List<string>();
             messages.Add(result);
-            messages.Add(result);
             messageBrokerMock.Setup(x => x.Send<string>(Environment.GetEnvironmentVariable("MP_CONSUMERQUEUE"), messages));
-
             int count = 15;
+           
             //Act
             int i = app.Run(interpreterPath, count);
-            int i2 = app.Run(interpreterPath2, count);
-            int i3 = app.Run(interpreterPath3, count);
+           
             //Assert
-            Assert.True(i>=0);
-            Assert.True(i2>= 0);
-            Assert.True(i3>= 0);
-            //Assert.True(script7.HasErrors);
+            Assert.True(i==0);
+        }
+
+        //This test tests a list with a error script.
+        [Fact]
+        public void Runtest2() {
+            //Arrange
+            List<Script> scripts2 = new List<Script>();
+            //wrong script
+            Script script2 = new Script() {
+                Id = "2",
+                Name = "javascript1",
+                Customer = "testCustomer",
+                ScriptVersion = "3.11",
+                Language = "javascript",
+                LanguageVersion = "1.0.1",
+                Code = "//This is the datamodel for Location, Source, and State." + "\n" +
+                     "let location = {" + "\n" +
+                     "   Id:" + '\u0022' + "" + '\u0022' + "," + "\n" +
+                     "   ParentId:" + '\u0022' + "" + '\u0022' + "," + "\n" +
+                     "wrong" +
+                     "};",
+                DateCreated = DateTime.Now,
+                Author = "test1",
+                LastModified = DateTime.Now
+            };
+            scripts2.Add(script2);
+            string queueName = Environment.GetEnvironmentVariable("MP_QUEUENAME");
+            messageBrokerMock.Setup(x => x.Receive(queueName)).Returns(scripts2);
+            string path2 = $@"c:\scripts\javascript\{script2.Id}.js";
+            Dictionary<string, string> paths2 = new Dictionary<string, string>();
+            paths2.Add(script2.Id, path2);
+            stagerMock.Setup(x => x.GetPaths(scripts2)).Returns(paths2);
+            var result2 = "wrong";
+            var interpreterPath2 = "node";
+            scriptRunnerMock.Setup(x => x.RunScript(script2.Id, path2, interpreterPath2)).Returns(result2);
+            dataValidationMock.Setup(x => x.ValidateScriptOutput(result2)).Throws(new ScriptFailedException());
+            dbAccessMock.Setup(x => x.Upsert(script2)).Returns(script2);
+            List<string> messages = new List<string>();
+            messageBrokerMock.Setup(x => x.Send<string>(Environment.GetEnvironmentVariable("MP_CONSUMERQUEUE"), messages));
+            int count = 15;
+
+            //Act
+            int i2 = app.Run(interpreterPath2, count);
+            
+            //Assert        
+            Assert.True(script2.HasErrors);
+        }
+
+        //This test tests a list which is null.
+        [Fact]
+        public void RunTest3() {
+            //Arrange
+            List<Script> scripts3 = null;
+            string queueName = Environment.GetEnvironmentVariable("MP_QUEUENAME");
+            messageBrokerMock.Setup(x => x.Receive(queueName)).Returns(scripts3);
+            var interpreterPath = "python";
+            int count = 15;
+
+            //Act
+            int i3 = app.Run(interpreterPath, count);
+
+            //Assert
+            Assert.Equal(16, i3);
         }
     }
 
